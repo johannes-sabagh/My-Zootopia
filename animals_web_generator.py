61b930f1a49ac14373c9c6c9ba67de10cc414bbe
johannes-sabagh <johannes.sabagh@gmail.com>
2026-02-09 15:37:53 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,14 +6,35 @@ def load_data(file_path):
     return json.load(handle)
 
 animals_data = load_data("animals_data.json")
+output = ''
 for animal in animals_data:
     name = animal["name"]
     diet = animal["characteristics"]["diet"]
     locations = animal["locations"][0]
     animal_type = animal["characteristics"].get("type")
-    print(f"Name: {name})")
-    print(f"Diet: {diet}")
-    print(f"Location: {locations}")
+    output += f"Name: {name}\n"
+    output += f"Diet: {diet}\n"
+    output += f"Location: {locations}\n"
     if animal_type:
-        print(f"Type: {animal_type}")
-    print()
+        output += f"Type: {animal_type}\n"
+    output += "\n"
+
+
+def load_html(html_path):
+    """loads HTML file"""
+    with open(html_path, "r") as html_file:
+        return html_file.read()
+html_text = load_html("animals_template.html")
+
+def update_html(output, html_text):
+    """create new html from the template and the output from JSON"""
+    new_html_text = html_text.replace("__REPLACE_ANIMALS_INFO__", output)
+    with open("animals.html", "w") as new_html:
+        new_html.write(new_html_text)
+
+
+def main():
+    update_html(output, html_text)
+
+if __name__ == "__main__":
+    main()
