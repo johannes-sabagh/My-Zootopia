585e0c701832a6e7e1a82a3fb2525808d436f596
johannes-sabagh <johannes.sabagh@gmail.com>
2026-02-09 15:59:29 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,37 +5,42 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-animals_data = load_data("animals_data.json")
-output = ''
-for animal in animals_data:
-    name = animal["name"]
-    diet = animal["characteristics"]["diet"]
-    locations = animal["locations"][0]
-    animal_type = animal["characteristics"].get("type")
-    output += '<li class="cards__item">'
-    output += f"Name: {name}<br/>\n"
-    output += f"Diet: {diet}<br/>\n"
-    output += f"Location: {locations}<br/>\n"
-    if animal_type:
-        output += f"Type: {animal_type}<br/>\n"
-    output += '</li>'
 
-
+def create_html():
+    animals_data = load_data("animals_data.json")
+    output = ''
+    for animal in animals_data:
+        name = animal["name"]
+        diet = animal["characteristics"]["diet"]
+        locations = animal["locations"][0]
+        animal_type = animal["characteristics"].get("type")
+        output += '<li class="cards__item">'
+        output += f'<div class="card__title">{name}</div>\n'
+        output += '<p class="card__text">'
+        output += f'<strong>Diet: </strong>{diet}<br/>\n'
+        output += f'<strong>Location: </strong>{locations}<br/>\n'
+        if animal_type:
+            output += f'<strong>Type: </strong>{animal_type}<br/>\n'
+        output += '</p>'
+        output += '</li>'
+    return output
+html_output = create_html()
 def load_html(html_path):
     """loads HTML file"""
     with open(html_path, "r") as html_file:
         return html_file.read()
 html_text = load_html("animals_template.html")
 
-def update_html(output, html_text):
+def write_html(html_output, html_text):
     """create new html from the template and the output from JSON"""
-    new_html_text = html_text.replace("__REPLACE_ANIMALS_INFO__", output)
+    new_html_text = html_text.replace("__REPLACE_ANIMALS_INFO__", html_output)
     with open("animals.html", "w") as new_html:
         new_html.write(new_html_text)
 
 
 def main():
-    update_html(output, html_text)
+    create_html()
+    write_html(html_output, html_text)
 
 if __name__ == "__main__":
     main()
